62c4042b0de20e31824ceae3b1e43721e961bf11
Giacomo Pignoni <giacomo.pignoni@gmail.com>
2024-06-05 14:13:16 +0200

    fix remove html

--- lib/permission_handler_web.dart
+++ lib/permission_handler_web.dart
@@ -1,10 +1,4 @@
 import 'dart:async';
-// In order to *not* need this ignore, consider extracting the "web" version
-// of your plugin as a separate package, instead of inlining it in the same
-// package as the core of your plugin.
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:permission_handler_platform_interface/permission_handler_platform_interface.dart';
@@ -115,9 +109,7 @@ class PermissionHandlerWeb extends PermissionHandlerPlatform {
   ///
   /// Returns a [Map] containing the status per requested [Permission].
   @override
-  Future<Map<Permission, PermissionStatus>> requestPermissions(
-      List<Permission> permissions) async {
-    final permObj = html.window.navigator.permissions!;
+  Future<Map<Permission, PermissionStatus>> requestPermissions(List<Permission> permissions) async {
     final Map<Permission, PermissionStatus> result = {};
     for (final perm in permissions) {
       result[perm] = PermissionStatus.granted;
